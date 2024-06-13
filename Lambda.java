342302eb06001d86f0d9d7141f63ee18ee618281
Shivraj2610 <shivrajnalavade17@gmail.com>
2024-06-13 23:59:12 +0530

    Revision of Lambda Expression

---
+++ Lambda.java
@@ -0,0 +1,21 @@
+
+@FunctionalInterface
+interface Demo {
+    void show();
+}
+
+public class Lambda implements Demo {
+    public void show() {
+        System.out.println("This is show");
+    }
+
+    public static void main(String[] args) {
+        // Without Lambda Expression
+        Lambda l = new Lambda();
+        l.show();
+
+        // By using Lambda Expression
+        Demo d = () -> System.out.println("Now I able to use lambda expression");
+        d.show();
+    }
+}
---
+++ LambdaWithArguments.java
@@ -0,0 +1,20 @@
+
+interface Apple {
+    public abstract int sum(int a, int b);
+}
+
+interface Ball {
+    public abstract int demo(int a);
+}
+
+public class LambdaWithArguments {
+    public static void main(String[] args) {
+        Apple l = (a, b) -> a + b;
+
+        int ans = l.sum(2, 9);
+        System.out.println(ans);
+
+        Ball b = a -> a / 2;
+        System.out.println(b.demo(ans));
+    }
+}
